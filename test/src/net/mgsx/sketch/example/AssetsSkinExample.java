18d8164f6c3d69601d7d8c711db7686215c9adc1
Germain AUBERT <germain.aubert@gmail.com>
2017-12-01 14:06:35 +0100

    added animations from blender

--- test/src/net/mgsx/sketch/example/AssetsSkinExample.java
+++ test/src/net/mgsx/sketch/example/AssetsSkinExample.java
@@ -6,11 +6,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class AssetsSkinExample extends ApplicationAdapter 
@@ -24,14 +29,18 @@ public class AssetsSkinExample extends ApplicationAdapter
 	
 	private Stage stage;
 	private Skin skin;
+	private Image animImage;
+	private float time;
+	private TextureRegionDrawable drawable;
+	private Animation<TextureRegion> anim;
 	
 	@Override
 	public void create () {
 		stage = new Stage(new ScreenViewport());
 		Gdx.input.setInputProcessor(stage);
 		skin = new Skin(Gdx.files.internal("assets/skins/hud-skin.json"));
+		
 		Table table = new Table(skin);
-		table.setFillParent(true);
 		table.add("Hello LD40!").row();
 		table.add(new TextButton("Button", skin)).row();
 		table.add(new Image(skin.getDrawable("noise"))).row();
@@ -46,20 +55,32 @@ public class AssetsSkinExample extends ApplicationAdapter
 		
 		subTable.add(new Image(new Texture(Gdx.files.internal("assets/bg.png")))).size(100, 100).row();
 		
-		stage.addActor(table);
+		TextureAtlas animAtlas = new TextureAtlas(Gdx.files.internal("assets/animations.atlas"));
+		anim = new Animation<TextureRegion>(.1f, animAtlas.findRegions("anim"));
+		
+		subTable.add(animImage = new Image(anim.getKeyFrame(0))).row();
+		drawable = new TextureRegionDrawable();
+		
+		Table root = new Table();
+		root.setFillParent(true);
+		root.add(table).expand().center();
+		stage.addActor(root);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		time += Gdx.graphics.getDeltaTime();
+		drawable.setRegion(anim.getKeyFrame(time, true));
+		animImage.setDrawable(drawable);
 		stage.act();
 		stage.draw();
 	}
 	
 	@Override
 	public void resize(int width, int height) {
-		stage.getViewport().update(width, height);
+		stage.getViewport().update(width, height, true);
 	}
 	
 }
